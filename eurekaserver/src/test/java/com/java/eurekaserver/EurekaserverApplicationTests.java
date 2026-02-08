7d12f60165d7952346abfdf229a0888f3dd3d683
tejasvisi <159748644+tejasvisi@users.noreply.github.com>
2026-02-08 11:41:53 +0530

    Add files via upload

---
+++ eurekaserver/src/test/java/com/java/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
